dd4ce5df892eaeba17cdb975df022b8bb7902b94
ghina <baassiri.ghina@gmail.com>
2021-06-22 14:50:03 +0300

    Fix minor stylings.

--- client-frontend/src/screens/ProductMarketsScreen.js
+++ client-frontend/src/screens/ProductMarketsScreen.js
@@ -1,30 +1,31 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { Text, View, StyleSheet, Image, FlatList, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet, Image, FlatList, TouchableOpacity, LogBox } from 'react-native';
 import { AuthContext } from '../navigation/AuthProvider';
 import { CommonScreenStyles } from '../utilities/Styles';
 import { ListStyles } from '../utilities/Styles';
 import Feather from 'react-native-vector-icons/Feather';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+// import { NavigationActions } from 'react-navigation';
+
 import axios from 'axios'
 
-export default function ProductMarketsScreen({route}) {
-
+export default function ProductMarketsScreen({route, navigation}) {
+  LogBox.ignoreAllLogs();
+  
   const { token } = useContext(AuthContext)
-  const [product, setProduct] = useState({});
   const [markets, setMarkets] = useState([]);
 
-  console.log('props ', route.params.productBarcode)
+  const product = route.params.product
+  let market = {}
 
-  function fetchProductMarkets(barcode) {
-    console.log('fetchProductMarkets barcode: ', barcode)
-    axios.get(`http://192.168.43.152:8000/api/productMarkets/`+ barcode, {
+
+  function fetchProductMarkets(product) {
+    axios.get(`http://192.168.43.152:8000/api/productMarkets/${product.barcode}`, {
       headers: {
         'Authorization': `Bearer ${token}`
       }}).then(response => {        
-        console.log('SUCCESS-Data: ', response.data)
         if(response.status === 200) { 
-          setMarkets(response.data.productMarketsDetails.markets)
-          setProduct(response.data.productMarketsDetails.product)
+          setMarkets(response.data.markets)
           return
         }      
     }).catch(err => {
@@ -34,21 +35,20 @@ export default function ProductMarketsScreen({route}) {
   }
 
   useEffect(() => {
-    fetchProductMarkets(route.params.productBarcode)      
+    fetchProductMarkets(product)      
   }, [])
 
+
   return (
-    <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
-      {console.log('product: ',product)}
-      {console.log('markets: ', markets)}
+    <View style={{flex:1, alignItems:'center', justifyContent:'center' }}>
       {/* Product */}
-      <View style={{alignItems:'center', justifyContent:'center', flex:0.2, paddingTop: 90}}>                           
+      <View style={ListStyles.mainItemWrapper}>                           
           <Image
-            source={{uri:product.image }} 
+            source={{uri:product.image}} 
             resizeMode='contain'
             style={ styles.image }
           />   
-          <Text style={{ color:'#000', fontSize:20, marginTop:7}}>{product.brand}</Text>
+          <Text style={{ color:'#000', fontSize:20, marginTop:7, fontWeight:'bold'}}>{product.brand}</Text>
       
           <Text style={{marginTop:4}}>{product.category}</Text>
 
@@ -56,61 +56,32 @@ export default function ProductMarketsScreen({route}) {
       </View>
 
       {/* Price per market */}
-      <View style={{flex:0.8, }}>
+        <View style={{...ListStyles.listWidth, flexDirection:'row',  justifyContent: 'center', borderWidth:1, borderColor:'#d4d4d4', borderTopLeftRadius:10, borderTopRightRadius: 10, backgroundColor:'#1eb980', height:35,  elevation:3 }}>
+          <Text style={{textAlign: 'center', alignSelf: 'center', fontSize:18 , fontWeight:'bold', borderRightWidth:1, width:'65%',  borderColor:'#d4d4d4', color:'#fff' }} >Market</Text>
+          <Text style={{textAlign: 'center', alignSelf: 'center', fontSize:18,  fontWeight:'bold', width:'35%', color: '#fff' }} >Price</Text>
+        </View>
         <FlatList
-          keyExtractor={item => item.id.toString()}
+          keyExtractor={(item, index) => index.toString()}
           data={markets}  
             renderItem={item => {
               return (
-                <View  style={ ListStyles.itemWrapper } >
-                    <View>                         
-                      <Image
-                        source={{uri:item.item.logo }} 
-                        resizeMode='contain'
-                        style={ ListStyles.image }
-                      />    
+                <View style={{...ListStyles.listWidth, flexDirection:'row', textAlign: 'center', alignItems:'center', justifyContent: 'center', borderWidth:1, borderColor:'#d4d4d4', backgroundColor:'#fff', height:60 }}>
+                    <TouchableOpacity 
+                      style={{ alignSelf: 'center', width:ListStyles.listWidth.width*0.64,  justifyContent: 'center', height:'100%', paddingHorizontal:4 }}
+                      onPress={() => {
+                        navigation.navigate('MarketStack', {screen: 'MarketProducts', params: {market: item.item}})}}
+                    >
+                      <Text style={{ height:'auto', textAlign: 'center',fontSize:16 , color:'#000'}} >{item.item.name}</Text> 
+                    </TouchableOpacity>
+                    <View  style={{ justifyContent: 'center', alignItems: 'center', width:'35%', height:'100%', borderLeftWidth:1, borderColor:'#d4d4d4' }} >
+                      <Text style={{ textAlign: 'center', fontSize:16, }} >{item.item.price}</Text>
                     </View>
-                    <View style={{flex: 1}}>
-
-                    <Text style={{ color:'#000', fontSize:20, marginBottom:10}}>{item.item.name}</Text>
-                    
-                    <Text style={{ color:'#000', fontSize:15, marginBottom:5}}>Price: {item.item.price} LBP</Text>
-
-                    <Text style={{ color:'#000', fontSize:15, marginBottom:5}}>{item.item.phone.slice(0, 2) +' '+item.item.phone.slice(2, 9) }</Text>
-                    <Text style={{ color:'#000', fontSize:15, marginBottom:5}}>{item.item.address.address }</Text>
-
-                    <View style={{flexDirection:'row'}}>
-                      <Text style={{marginBottom:5}}>Delivery: </Text> 
-                      {item.item.delivery ? 
-                        <Feather
-                          name='check-circle'
-                          size={15}
-                          color='green'
-                          style={{top:3}}
-                        /> : 
-                        <Feather
-                          name='x-circle'
-                          size={17}
-                          color='#cf4332'
-                          style={{top:1}}
-                        />}
-                      
-                    </View>
-                    
-                    <View style={{flexDirection:'row', marginBottom:5}}>
-                        {console.log('Rating',item.item.rating)}
-                        {<RatingView numOfStars={item.item.rating}/>}                  
-                    </View>              
-                </View>
-        
-                </View>
-              )}}
-                      
+                  </View>
+              )}}                      
               showsVerticalScrollIndicator={false}              
-              contentContainerStyle={{paddingTop:50,paddingBottom:65}}
+              contentContainerStyle={{paddingTop:1,paddingBottom:65}}
         />
       </View>
-    </View>
   )
 }
 
@@ -121,38 +92,3 @@ const styles = StyleSheet.create({
     height:120
   }
 })
-
-
-function RatingView(numOfStars) {
-  var stars=[]
-    // console.log("PROP ",numOfStars.numOfStars)
-    if(numOfStars.numOfStars > 0) {
-      for (let i = 0; i < numOfStars.numOfStars; i++) {
-        stars.push(
-          <Image 
-            source={require('../../assets/star.png')} 
-            resizeMode='contain'
-            style={{width:19, height:19, marginRight:2}}
-          />
-        )        
-      }
-    }
-    if(numOfStars.numOfStars != 5) {
-      for (let i = 0; i < 5-numOfStars.numOfStars; i++) {
-        // console.log('empty star')
-        stars.push(
-          <Image 
-            source={require('../../assets/star_outline.png')} 
-            resizeMode='contain'
-            style={{width:19, height:19,marginRight:2, tintColor:'#a9a9a9'}}
-          />
-        )
-      }
-    }
-    // console.log('Star', stars.length)
-  return (
-    <View style={{flexDirection:'row'}}>
-      {stars}
-    </View>
-  )  
-}
