ff1c7a7d342f5dec77f634c40149e94bf85b0319
ghina <baassiri.ghina@gmail.com>
2021-07-03 18:48:43 +0300

    Add header for market stack screens.

--- client-frontend/src/navigation/MarketStack.js
+++ client-frontend/src/navigation/MarketStack.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import { TouchableOpacity, Text } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import MarketProductsScreen from '../screens/MarketProductsScreen';
 import MapScreen from '../screens/MapScreen';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 const MarketStackNavigation = createStackNavigator();
 
-export default function MarketStack() {
+export default function MarketStack({navigation}) {
 
   return (
-    <MarketStackNavigation.Navigator initialRouteName='Map'>
+    <MarketStackNavigation.Navigator 
+      initialRouteName='Map'
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#1eb980',
+        },
+        headerTintColor: '#fff',
+        headerStatusBarHeight:25,
+        headerTitleAlign: 'center',         
+      }}
+    >
       <MarketStackNavigation.Screen 
         name="Map" 
         component={MapScreen} 
@@ -17,9 +29,22 @@ export default function MarketStack() {
       <MarketStackNavigation.Screen 
         name="MarketProducts" 
         component={MarketProductsScreen} 
-        options={{ header: () => null }} 
+        options={{
+          title: '',
+          headerRight:false,
+          headerLeft: ()=>(
+            <TouchableOpacity onPress={() => navigation.navigate('Map')} style={{flexDirection:'row', top:5}}> 
+              <MaterialIcons
+                name='navigate-before'
+                color='#fff'
+                size={24}   
+                style={{paddingLeft:10}}                    
+              />
+              <Text style={{fontSize:15, color:'#fff', top:2}}>Markets</Text> 
+            </TouchableOpacity>            
+          )      
+        }}
       />    
     </MarketStackNavigation.Navigator>
   );
 }
-
