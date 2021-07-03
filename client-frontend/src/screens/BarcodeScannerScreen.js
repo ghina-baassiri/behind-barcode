043f9cfc9fde9a4af72fbedeb5f8e1af2f7aa087
ghina <baassiri.ghina@gmail.com>
2021-07-03 18:32:38 +0300

    Fix barcode scanning functionality.

--- client-frontend/src/screens/BarcodeScannerScreen.js
+++ client-frontend/src/screens/BarcodeScannerScreen.js
@@ -1,22 +1,65 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Text, View, StyleSheet, Button } from 'react-native';
+import { AuthContext } from '../navigation/AuthProvider';
 import { BarCodeScanner } from 'expo-barcode-scanner';
+import { Camera } from 'expo-camera';
 import { CommonScreenStyles } from '../utilities/Styles';
+import { screenHeight } from '../utilities/Dimensions';
+import BarcodeScannerModal from '../components/BarcodeScannerModal';
+import ProductNotFoundModal from '../components/ProductNotFoundModal';
+import {useIsFocused} from '@react-navigation/native';
+import axios from 'axios'
 
-export default function BarcodeScannerScreen() {
+export default function BarcodeScannerScreen({navigation}) {
   const [hasPermission, setHasPermission] = useState(null);
+  const [failed, setFailed] = useState(false);
   const [scanned, setScanned] = useState(false);
+  const [product, setProduct] = useState(null);
+  const [rerender, setRerender] = useState(true);
+
+  const [showProductModal, setShowProductModal] = useState(false);
+  const [showProductNotFoundModal, setShowProductNotFoundModal] = useState(false);
+  const { token } = useContext(AuthContext);
+  const isFocused = useIsFocused();
+ 
 
   useEffect(() => {
-    (async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })();
-  }, []);
+    console.log('isFocused: ', isFocused)
+    console.log('failed: ', failed)  
+    console.log('scanned: ', scanned)  
+    console.log('error product: ', product)    
+    console.log('showProductNotFoundModal: ', showProductNotFoundModal)
 
-  const handleBarCodeScanned = ({ type, data }) => {
-    setScanned(true);
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    if(!isFocused){
+      setShowProductModal(false)
+      setScanned(false) 
+      setProduct(null)
+    } else {
+      if (rerender) {
+        (async () => {
+          // Camera access permission check
+          const cameraStatus = await Camera.requestPermissionsAsync();
+          setHasPermission(cameraStatus.status === 'granted');
+        })()
+        setShowProductModal(false)
+        setScanned(false) 
+        setProduct(null)
+        setRerender(false)
+      }
+      if(failed) {
+        setShowProductNotFoundModal(true) 
+        setScanned(true)
+        setProduct(null)   
+        setFailed(false) 
+      }
+    }
+  })
+
+  
+
+  const handleBarCodeScanned = ({ data }) => {
+    setScanned(true)
+    fetchProduct(data)
   };
 
   if (hasPermission === null) {
@@ -26,13 +69,44 @@ export default function BarcodeScannerScreen() {
     return <Text>No access to camera</Text>;
   }
 
+
+  const fetchProduct = (barcode) => {
+    console.log('fetch')
+    axios.get(`http://192.168.43.152:8000/api/product/${barcode}`, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }})
+    .then(response => { 
+      if(response.data.product) {
+        setProduct(response.data.product)
+        setShowProductModal(true)
+        return
+      }
+    })
+    .catch(err => {
+      // alert('Scan again')
+      setFailed(true) 
+      console.log('Fetch Product Error: ', err)      
+    })   
+  } 
+
   return (
     <View style={CommonScreenStyles.container}>
-      <BarCodeScanner
-        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-        style={StyleSheet.absoluteFillObject}
-      />
-      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
-    </View>
+      { scanned ?
+        <>
+          {product ?
+          <BarcodeScannerModal product={product} visibility={showProductModal} setVisibility={setShowProductModal} setScanned={setScanned} setProduct={setProduct} navigation={navigation}/>
+          :
+          <ProductNotFoundModal visibility={showProductNotFoundModal} setVisibility={setShowProductNotFoundModal} setScanned={setScanned}/>
+          }
+        </>
+        :
+          <Camera
+            onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+            ratio='16:9'
+            style={StyleSheet.absoluteFillObject}
+          /> 
+          }
+    </View> 
   );
 }
