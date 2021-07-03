a3748bfac73b9e8ea6e345ec37a9b90b43d8e12b
ghina <baassiri.ghina@gmail.com>
2021-07-03 18:36:43 +0300

    Add fetch if user rated a market function.

--- client-frontend/src/screens/MarketProductsScreen.js
+++ client-frontend/src/screens/MarketProductsScreen.js
@@ -1,17 +1,40 @@
 import React, { useState, useEffect, useContext } from 'react'
-import { Text, View, StyleSheet, Image, FlatList, TouchableOpacity, LogBox } from 'react-native';
+import { Text, View, StyleSheet, Image, FlatList, TouchableOpacity, LogBox, ScrollView } from 'react-native';
 import { AuthContext } from '../navigation/AuthProvider';
-import { CommonScreenStyles, ListStyles } from '../utilities/Styles';
+import { ListStyles, LoginScreenStyles } from '../utilities/Styles';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import axios from 'axios';
 
 export default function MarketProductsScreen({ route, navigation }) {
   LogBox.ignoreAllLogs();
 
-  const { token } = useContext(AuthContext)
+  const { user, token } = useContext(AuthContext)
   const [products, setProducts] = useState([]);
+  const [productName, setProductName] = useState('');
+  const [productCategory, setProductCategory] = useState('');
+  const [rated, setRated] = useState(false)
+
 
   const market = route.params.market
+  console.log('market is:',market)
   let product = {}
+
+  const fetchRated = (userId, marketId) => {
+    axios.get(`http://192.168.43.152:8000/api/rated/${marketId}/user/${userId}`, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }})
+    .then(response => {        
+        console.log('rated: ', response.data.rated)
+
+        setRated(response.data.rated)
+        return
+    })
+    .catch(err => {
+    console.log('rated Error: ', err)      
+    })   
+}
+
 
   function fetchMarketProducts(market) {
     axios.get(`http://192.168.43.152:8000/api/marketProducts/${market.id}`, {
@@ -23,63 +46,80 @@ export default function MarketProductsScreen({ route, navigation }) {
           return
         }      
     }).catch(err => {
-      console.log('The Error: ', err)
+      console.log('fetchMarketProducts Error: ', err)
       
     })   
   }
 
   useEffect(() => {
+    fetchRated(user._id, market.id)
     fetchMarketProducts(market)      
   }, [])
 
   return (
-    <View style={{flex:1, alignItems:'center', justifyContent:'center' }}>
+    <View style={{flex:1, alignItems:'center', }}>
       {/* Market */}
-      <View style={ListStyles.mainItemWrapper}>                           
+      <View style={{...ListStyles.mainItemWrapper, paddingHorizontal:15, marginTop:20}}>                           
           <Image
             source={{uri:market.logo}} 
             resizeMode='contain'
             style={styles.image}
           />   
           <Text style={{ color:'#000', fontSize:16, marginBottom:10, fontWeight:'bold'}}>{market.name}</Text>
-          <Text style={{color:'#000', fontSize:13, marginBottom:2, textAlign:'center'}}>{market.phone.slice(0, 2) +' '+market.phone.slice(2, 9) }</Text>
-          <TouchableOpacity onPress={() => navigation.navigate('Map',{address:market.address})}>
+          <TouchableOpacity activeOpacity={.9} onPress={() => navigation.navigate('Map',{address:market.address})}>
             <Image
               source={require('../../assets/map.png')}
               resizeMode='contain'
               style={styles.icon}
             />
           </TouchableOpacity>
+          <Text style={{color:'#000', fontSize:13, marginBottom:2, textAlign:'center'}}>{market.phone.slice(0, 2) +' '+market.phone.slice(2, 9) }</Text>
+          
           <Text style={{marginBottom:4, fontSize:13, textAlign:'center'}}>{market.address.address}</Text>
 
           <View style={{flexDirection:'row', marginVertical:5}}>
               <RatingView numOfStars={market.rating}/>                
-          </View>       
+          </View>     
+          <View>
+            {!rated && <TouchableOpacity style={styles.btn} onPress={()=> {}}>
+              <Text style={styles.btnText}>Rate</Text>
+            </TouchableOpacity> }
+          </View>  
       </View>
 
       {/* Products prices */}
         <View style={{...ListStyles.listWidth, flexDirection:'row',  justifyContent: 'center', borderWidth:1, borderColor:'#d4d4d4', borderTopLeftRadius:10, borderTopRightRadius: 10, backgroundColor:'#1eb980', height:35,  elevation:3 }}>
-          <Text style={{textAlign: 'center', alignSelf: 'center', fontSize:18 , fontWeight:'bold', borderRightWidth:1, width:'65%',  borderColor:'#d4d4d4', color:'#fff' }} >Product</Text>
-          <Text style={{textAlign: 'center', alignSelf: 'center', fontSize:18,  fontWeight:'bold', width:'35%', color: '#fff' }} >Price</Text>
+          <Text style={{textAlign: 'center', alignSelf: 'center', fontSize:18 , fontWeight:'bold', borderRightWidth:1, width:'68%',  borderColor:'#d4d4d4', color:'#fff' }} >Product</Text>
+          <View style={{width:'32%', alignItems: 'center', justifyContent:'center', flexDirection:'row'}}>
+              <Text style={{  textAlign:'center', fontSize:18,  fontWeight:'bold', color: '#fff' }} >Price </Text>
+              <Text style={{  textAlign:'center', fontSize:14,  fontWeight:'bold', color: '#fff' }} >(LBP)</Text>
+            </View>
         </View>
         <FlatList
           keyExtractor={(item, index) => index.toString()}
           data={products}  
             renderItem={item => {
-              product = item.item
 
               return (
                   <View style={{...ListStyles.listWidth, flexDirection:'row', textAlign: 'center', alignItems:'center', justifyContent: 'center', borderWidth:1, borderColor:'#d4d4d4', backgroundColor:'#fff', height:60 }}>
                     <TouchableOpacity 
-                      style={{ alignSelf: 'center', width:ListStyles.listWidth.width*0.64,  justifyContent: 'center', height:'100%', paddingHorizontal:4 }}
-                      onPress={() => {navigation.navigate('ProductStack', {screen: 'ProductMarkets', params: {product:product}})}}
-                    >
-                      <Text style={{  height:'auto', textAlign: 'center',fontSize:16 , color:'#000', borderBottomColor:"#1eb980"}} >{product.brand} </Text> 
-                      <Text style={{  height:'auto', textAlign: 'center',fontSize:13 , color:'#000', fontStyle: 'italic'}}>{product.size} {product.unit}</Text> 
-
+                      style={{ flexDirection:'row', alignItems:'center', alignSelf: 'center', width:ListStyles.listWidth.width*0.68,  justifyContent: 'center', height:'100%', paddingHorizontal:2 }}
+                      onPress={() => {navigation.navigate('ProductStack', {screen: 'ProductMarkets', params: {product:item.item}})}}
+                    > 
+                      <View>
+                        <Text style={{  height:'auto', textAlign: 'center',fontSize:16 , color:'#000', borderBottomColor:"#1eb980"}} >{item.item.brand} </Text> 
+                        <Text style={{  height:'auto', textAlign: 'center',fontSize:13 , color:'#000', fontStyle: 'italic'}}>{item.item.size} {item.item.unit}</Text> 
+                      </View>
+                      <MaterialIcons
+                        name='navigate-next'
+                        color='#000'
+                        size={20}   
+                        style={{paddingLeft:5, position:'absolute',right:3, bottom:3}}                    
+                      />
                     </TouchableOpacity>
-                    <View  style={{ justifyContent: 'center', alignItems: 'center', width:'35%', height:'100%', borderLeftWidth:1, borderColor:'#d4d4d4' }} >
-                      <Text style={{ textAlign: 'center', fontSize:16 }} >{product.price}</Text>
+                    <View  style={{ justifyContent: 'center', alignItems: 'center', width:'32%', height:'100%', borderLeftWidth:1, borderColor:'#d4d4d4' }} >
+                      <Text style={{ textAlign: 'center', fontSize:16 }} >{item.item.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</Text>
+                      
                     </View>
                     
                   </View>
@@ -87,7 +127,7 @@ export default function MarketProductsScreen({ route, navigation }) {
               showsVerticalScrollIndicator={false}              
               contentContainerStyle={{paddingTop:1,paddingBottom:65}}
         />
-      </View>
+        </View>
   );
 }
 
@@ -99,11 +139,25 @@ const styles = StyleSheet.create({
     borderRadius:10
   },
   icon: {
-    width:35, 
-    height:35,
+    width:30, 
+    height:30,
     marginBottom:4,
     borderRadius:10
-  }
+  },
+  btn: {
+    width:  60,
+    height: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    marginTop:15,
+    backgroundColor:'#1eb980'
+},
+btnText: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    color:'#fff'
+}, 
 })
 
 function RatingView(numOfStars) {
@@ -113,6 +167,7 @@ function RatingView(numOfStars) {
       for (let i = 0; i < numOfStars.numOfStars; i++) {
         stars.push(
           <Image 
+            key={'stars_' + i}
             source={require('../../assets/star.png')} 
             resizeMode='contain'
             style={{width:19, height:19, marginRight:2, tintColor:'#1eb980'}}
@@ -125,6 +180,7 @@ function RatingView(numOfStars) {
         // console.log('empty star')
         stars.push(
           <Image 
+            key={'stars_empty' + i}
             source={require('../../assets/star_outline.png')} 
             resizeMode='contain'
             style={{width:19, height:19,marginRight:2, tintColor:'#a9a9a9'}}
