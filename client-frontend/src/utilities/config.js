b6882bafb13f6ecd0aa44ddc79c875fced792b6c
Ghina Baassiri <82706150+ghina-baassiri@users.noreply.github.com>
2021-07-10 19:49:35 +0300

    Update config.js

--- client-frontend/src/utilities/config.js
+++ client-frontend/src/utilities/config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: 'AIzaSyDAivsUzas4Z9b14S0jxaoah2zGGx_WSHA',
-  authDomain: 'behind-barcode.firebaseapp.com',
-  projectId: 'behind-barcode',
-  storageBucket: 'behind-barcode.appspot.com',
-  messagingSenderId: '480090643589',
-  appId: '1:480090643589:web:8981da19a2808075dc5dec',
-  measurementId: 'G-S9H74RG340',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
 }
